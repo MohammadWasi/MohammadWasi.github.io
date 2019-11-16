37ef732dd5bbde997eafc4012477e968c9ac1219
Mohammad Wasi <mdwasi15@gmail.com>
2019-11-16 19:35:53 +0530

    first deploy

---
+++ build/precache-manifest.9f04c7663a8cf1e81eb24e3e505bb3fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55edf108b4c9fd870fc7a4cd6f2176a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da863257c74c78aff8e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7949fd778a874c6f9920",
+    "url": "/static/js/2.e68ede77.chunk.js"
+  },
+  {
+    "revision": "8da863257c74c78aff8e",
+    "url": "/static/js/main.21328a04.chunk.js"
+  },
+  {
+    "revision": "bfd1521e694d93483f19",
+    "url": "/static/js/runtime-main.14a2f347.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
